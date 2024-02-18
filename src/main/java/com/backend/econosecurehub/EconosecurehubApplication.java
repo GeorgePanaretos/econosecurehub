0adb061fe8eb93ae113a2d3678738f3f2f505368
George Panaretos <georgepanaretos.cac@gmail.com>
2024-02-18 12:03:33 +0100

    First commit

---
+++ src/main/java/com/backend/econosecurehub/EconosecurehubApplication.java
@@ -0,0 +1,13 @@
+package com.backend.econosecurehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconosecurehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconosecurehubApplication.class, args);
+	}
+
+}
