559b53c53d1607731c2784ae082a6036ce1a4381
George Panaretos <georgepanaretos.cac@gmail.com>
2024-02-18 12:04:46 +0100

    Database schema

---
+++ src/main/java/com/backend/econosecurehub/repository/implementation/UserRepositoryImpl.java
@@ -0,0 +1,50 @@
+package com.backend.econosecurehub.repository.implementation;
+
+import com.backend.econosecurehub.domain.User;
+import com.backend.econosecurehub.exception.ApiException;
+import com.backend.econosecurehub.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class UserRepositoryImpl implements UserRepository<User> {
+    private static final String COUNT_USER_EMAIL_QUERY = "";
+    public final NamedParameterJdbcTemplate jdbc;
+    @Override
+    public User create(User user) {
+        if(getEmailCount(user.getEmail().trim().toLowerCase()) > 0) throw new ApiException("Email already in use. Please use a different email and try again.");
+        return null;
+    }
+
+
+    @Override
+    public Collection<User> list(int page, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public User get(Long id) {
+        return null;
+    }
+
+    @Override
+    public User update(User data) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    private Integer getEmailCount(String email) {
+        return jdbc.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
+    }
+}
